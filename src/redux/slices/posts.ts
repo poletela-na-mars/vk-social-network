02a9b1c8beaeb8d866f3122a4ce572f4d0b9fbac
Polina <polina-postylyakova@yandex.ru>
2023-06-21 01:10:49 +0300

    rewrote data fetch with useSelector in Profile (likePost, updateUserData)

--- src/redux/slices/posts.ts
+++ src/redux/slices/posts.ts
@@ -1,9 +1,11 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from '../../axios';
 
+import { PostType } from '../../types/PostType';
+
 // @ts-ignore
 export const fetchPosts: any = createAsyncThunk('posts/fetchPosts', async ({mode}) => {
-  return axios.get('/posts', { params: { mode: mode } })
+  return axios.get('/posts', {params: {mode: mode}})
       .then((res) => {
         return res.data;
       })
@@ -14,7 +16,19 @@ export const fetchPosts: any = createAsyncThunk('posts/fetchPosts', async ({mode
 });
 
 // @ts-ignore
-export const fetchPost: any = createAsyncThunk('posts/fetchPosts', async ({id}) => {
+export const likePost: any = createAsyncThunk('posts/likePost', async ({postId, posts}) => {
+  return axios.patch(`/posts/${postId}`)
+      .then((res) => {
+        return posts.map((el: PostType) => (el._id === postId) && res.data);
+      })
+      .catch((err) => {
+        console.error(err.response.data);
+        return Promise.reject(JSON.stringify(err.response.data));
+      });
+});
+
+// @ts-ignore
+export const fetchPost: any = createAsyncThunk('posts/fetchPost', async ({id}) => {
   return axios.get(`/posts/${id}`)
       .then((res) => {
         return res.data;
@@ -26,10 +40,10 @@ export const fetchPost: any = createAsyncThunk('posts/fetchPosts', async ({id}) 
 });
 
 const initialState = {
-  posts: {
-    items: [],
-    status: 'loading',
-  }
+  post: null,
+  posts: [],
+  postStatus: 'loading',
+  postsStatus: 'loading',
 };
 
 const postsSlice = createSlice({
@@ -38,16 +52,20 @@ const postsSlice = createSlice({
   reducers: {},
   extraReducers: {
     [fetchPosts.pending]: (state) => {
-      state.posts.items = [];
-      state.posts.status = 'loading';
+      state.posts = [];
+      state.postsStatus = 'loading';
     },
     [fetchPosts.fulfilled]: (state, action) => {
-      state.posts.items = action.payload;
-      state.posts.status = 'loaded';
+      state.posts = action.payload;
+      state.postsStatus = 'loaded';
     },
     [fetchPosts.rejected]: (state) => {
-      state.posts.items = [];
-      state.posts.status = 'error';
+      state.posts = [];
+      state.postsStatus = 'error';
+    },
+    [likePost.fulfilled]: (state, action) => {
+      state.posts = action.payload;
+      state.postsStatus = 'loaded';
     }
   },
 });
